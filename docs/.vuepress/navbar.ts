66a27de59b5713fb71232bf8cff2df486e419136
Yevin Yu <yuwb0521@outlook.com>
2024-12-02 15:34:56 +0800

    [docs][20241202][文档同步]
    - 同步文档-H5点击背景色异常问题
    - 同步文档-个人网站建站教程
    - 同步文档-CSS字体轮播效果
    - 同步文档-CSS滚动条
    - 同步文档-事件总线使用
    - 同步文档-组件自我调用

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,24 +1,33 @@
-import { defineNavbarConfig } from 'vuepress-theme-plume'
+import { defineNavbarConfig } from "vuepress-theme-plume";
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: '/', icon: 'material-symbols:home' },
-  { text: '博客', link: '/blog/', icon: 'material-symbols:notes' },
+  { text: "首页", link: "/", icon: "material-symbols:home" },
+  { text: "博客", link: "/blog/", icon: "material-symbols:notes" },
   {
-    text: 'Web前端', icon: "icon-park-twotone:web-page", items: [
-      { text: 'JavaScript', link: '/JavaScript/21qygyp7/', icon: "cib:js" },
-      { text: 'Node.js', link: '/Node.js/cznd3edy/', icon: "teenyicons:nodejs-solid" },
-      { text: '计算机基础', link: '/Computer/r8g2shzj/', icon: "material-symbols:computer" },
-    ]
+    text: "Web前端",
+    icon: "icon-park-twotone:web-page",
+    items: [
+      { text: "CSS", link: "/CSS/b3trdf6x/", icon: "fa6-brands:css" },
+      { text: "JavaScript", link: "/JavaScript/21qygyp7/", icon: "cib:js" },
+      { text: "Vue2", link: "/Vue2/amnf6e15/", icon: "logos:vue" },
+      {
+        text: "Node.js",
+        link: "/Node.js/cznd3edy/",
+        icon: "teenyicons:nodejs-solid",
+      },
+      {
+        text: "计算机基础",
+        link: "/Computer/r8g2shzj/",
+        icon: "material-symbols:computer",
+      },
+    ],
   },
-  // {
-  //   text: '后端&服务', icon: "mdi:server-outline", items: [
-
-  //   ]
-  // },
   {
-    text: '插件&工具', icon: "fluent:window-dev-tools-16-filled", items: [
-      { text: 'NPM', link: '/NPM/b54c2715/', icon: "gg:npm" },
-      { text: 'Git', link: '/Git/h8hjd37u/', icon: "mdi:git" },
-    ]
+    text: "插件&工具",
+    icon: "fluent:window-dev-tools-16-filled",
+    items: [
+      { text: "NPM", link: "/NPM/b54c2715/", icon: "gg:npm" },
+      { text: "Git", link: "/Git/h8hjd37u/", icon: "mdi:git" },
+    ],
   },
-])
+]);
--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -1,71 +1,101 @@
-import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
+import { defineNoteConfig, defineNotesConfig } from "vuepress-theme-plume";
+
+const css = defineNoteConfig({
+  dir: "CSS",
+  link: "/CSS",
+  sidebar: [
+    {
+      text: "CSS Basic",
+      icon: "ic:baseline-dashboard",
+      items: ["CSS滚动条"],
+    },
+    {
+      text: "CSS Case",
+      icon: "mdi:jewel-case",
+      items: ["CSS文字轮播效果"],
+    },
+  ],
+});
 
 const javascript = defineNoteConfig({
-    dir: 'JavaScript',
-    link: '/JavaScript',
-    sidebar: [
-        {
-            text: 'JavaScript Basic',
-            icon: 'ic:baseline-dashboard',
-            items: ['WebSocket', 'Event Loop']
-        }
-    ]
-})
+  dir: "JavaScript",
+  link: "/JavaScript",
+  sidebar: [
+    {
+      text: "JavaScript Basic",
+      icon: "ic:baseline-dashboard",
+      items: ["WebSocket", "Event Loop"],
+    },
+  ],
+});
+
+const vue2 = defineNoteConfig({
+  dir: "Vue2",
+  link: "/Vue2",
+  sidebar: [
+    {
+      text: "Vue2 Case",
+      icon: "mdi:jewel-case",
+      items: ["事件总线使用", "组件自我调用"],
+    },
+  ],
+});
+
 const node = defineNoteConfig({
-    dir: 'Node.js',
-    link: '/Node.js',
-    sidebar: [
-        {
-            text: 'Node.js Basic',
-            icon: 'ic:baseline-dashboard',
-            items: ['Node.js版本管理-n', 'Node.js版本管理-nvm']
-        }
-    ]
-})
+  dir: "Node.js",
+  link: "/Node.js",
+  sidebar: [
+    {
+      text: "Node.js Basic",
+      icon: "ic:baseline-dashboard",
+      items: ["Node.js版本管理-n", "Node.js版本管理-nvm"],
+    },
+  ],
+});
 
 const npm = defineNoteConfig({
-    dir: 'NPM',
-    link: '/NPM',
-    sidebar: [
-        {
-            text: 'NPM Basic',
-            icon: 'ic:baseline-dashboard',
-            items: ['包管理器基础知识',  'Vue2组件封装与发布指南']
-        },
-        {
-            text: 'NPM Problem Solving',
-            icon: 'material-symbols:problem',
-            items: ['解决常见依赖安装问题']
-        }
-    ]
-})
+  dir: "NPM",
+  link: "/NPM",
+  sidebar: [
+    {
+      text: "NPM Basic",
+      icon: "ic:baseline-dashboard",
+      items: ["包管理器基础知识", "Vue2组件封装与发布指南"],
+    },
+    {
+      text: "NPM Problem Solving",
+      icon: "material-symbols:problem",
+      items: ["解决常见依赖安装问题"],
+    },
+  ],
+});
 
 const git = defineNoteConfig({
-    dir: 'Git',
-    link: '/Git',
-    sidebar: [
-        {
-            text: 'Git Basic',
-            icon: 'ic:baseline-dashboard',
-            items: ['Git基础命令', 'GitCommitMessage规范']
-        }
-    ]
-})
+  dir: "Git",
+  link: "/Git",
+  sidebar: [
+    {
+      text: "Git Basic",
+      icon: "ic:baseline-dashboard",
+      items: ["Git基础命令", "GitCommitMessage规范"],
+    },
+  ],
+});
 
 const computer = defineNoteConfig({
-    dir: 'Computer',
-    link: '/Computer',
-    sidebar: [
-        {
-            text: 'Computer Basic',
-            icon: 'ic:baseline-dashboard',
-            items: ['HTTPS原理解析', '浏览器是运作原理']
-        }
-    ]
-})
+  dir: "Computer",
+  link: "/Computer",
+  sidebar: [
+    {
+      text: "Computer Basic",
+      icon: "ic:baseline-dashboard",
+      items: ["HTTPS原理解析", "浏览器是运作原理"],
+    },
+  ],
+});
 
 export const notes = defineNotesConfig({
-    dir: 'notes',
-    link: '/',
-    notes: [javascript, node, npm, git,computer],
-})
+  dir: "notes",
+  link: "/",
+  notes: [css, javascript, vue2, node, npm, git, computer],
+});
