aab043f43a001714ed256eb88a5480c47e8479b6
Yevin Yu <yuwb0521@outlook.com>
2024-12-03 17:34:13 +0800

    [build][20241203][添加统计插件&兼容个人网站]
    - 添加统计插件
    - 兼容个人网站
    - 添加个人网站SEO

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,45 +1,70 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defineUserConfig } from 'vuepress'
-import { plumeTheme } from 'vuepress-theme-plume'
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defineUserConfig } from "vuepress";
+import { plumeTheme } from "vuepress-theme-plume";
 
 export default defineUserConfig({
   bundler: viteBundler(),
   // 网站信息
   base: '/yevin-notes-blog/', // Github Pages
-  lang: 'zh-CN',
-  title: '耶温笔记',
-  description: '耶温笔记-耶温博客-学习记录',
+  // base: "/",
+  lang: "zh-CN",
+  title: "耶温笔记",
+  description: "耶温笔记-耶温博客-学习记录",
   // 主题配置
   theme: plumeTheme({
     // seo地址
-    // hostname: 'https://www.yuwb.cn',
+    // hostname: "https://yuwb.cn",
     // 插件
     plugins: {
-      shiki: { // 代码高亮
-        languages: ['shell', 'bash', 'typescript', 'javascript', 'html', 'css', 'vue','nginx'],
+      shiki: {
+        // 代码高亮
+        languages: [
+          "shell",
+          "bash",
+          "typescript",
+          "javascript",
+          "html",
+          "css",
+          "vue",
+          "nginx",
+        ],
       },
-      markdownEnhance: { // 代码块
+      markdownEnhance: {
+        // 代码块
         demo: true,
       },
-      comment: {  // 评论
-        provider: 'Giscus', 
+      comment: {
+        // 评论
+        provider: "Giscus",
         comment: true,
-        repo: 'Yevin-Yu/blog-comments', 
-        repoId: 'R_kgDOMNskgg', 
-        category: 'Q&A', 
-        categoryId: 'DIC_kwDOMNskgs4CgWpW', 
-      }
+        repo: "Yevin-Yu/blog-comments",
+        repoId: "R_kgDOMNskgg",
+        category: "Q&A",
+        categoryId: "DIC_kwDOMNskgs4CgWpW",
+      },
     },
   }),
   // 头部内容
   head: [
-    ['link', {
-      rel: 'icon',
-      href: '/favicon.ico'
-    }],
-    ['meta', {
-      name: 'keywords',
-      href: '耶温笔记,日常记录,前端学习,前端笔记,问题记录,HTML,CSS,JavaScript,Vue,Node.js'
-    }],
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "keywords",
+        href: "耶温笔记,日常记录,前端学习,前端笔记,问题记录,HTML,CSS,JavaScript,Vue,Node.js",
+      },
+    ],
+    [
+      "script",
+      {
+        src:"/tongji.js" // 添加百度统计插件
+      }
+    ]
   ],
-})
+});
